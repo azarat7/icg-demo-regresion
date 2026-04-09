68207adea8f75907811ca1d215563e129cbab8f7
Alejandro Zarat <azarat@icg.com.gt>
2026-04-09 12:11:48 -0600

    Renombrar nombre de las evidencias

--- tests/regresion/autenticacion/test_login.py
+++ tests/regresion/autenticacion/test_login.py
@@ -10,20 +10,20 @@ def test_PRB3103_15_login_exitoso_con_credenciales_validas(page):
     # 1. Ir a la pantalla de login
     page.goto(f'{BASE}/index.html')
     page.wait_for_timeout(1000) # Pausa visual para el video
-    page.screenshot(path=str(EVIDENCIAS / 'PRB15_01_inicio.png'))
+    page.screenshot(path=str(EVIDENCIAS / 'PRB3103_01_inicio.png'))
 
     # 2. Ingresar credenciales
     page.fill('#usuario', 'analista@icg.com')
     page.fill('#password', 'Demo2026')
     page.wait_for_timeout(1000) # Pausa visual para el video
-    page.screenshot(path=str(EVIDENCIAS / 'PRB15_02_credenciales.png'))
+    page.screenshot(path=str(EVIDENCIAS / 'PRB3103_02_credenciales.png'))
 
     # 3. Clic en Ingresar
     page.click('#btn-login')
     
     # 4. Esperar navegación y validar
     page.wait_for_url('**/dashboard.html', timeout=5000)
-    page.screenshot(path=str(EVIDENCIAS / 'PRB15_03_dashboard.png'))
+    page.screenshot(path=str(EVIDENCIAS / 'PRB3103_03_dashboard.png'))
     
     assert 'Dashboard' in page.title()
     
--- tests/regresion/creditos/test_calculo_interes.py
+++ tests/regresion/creditos/test_calculo_interes.py
@@ -8,18 +8,18 @@ BASE = 'http://localhost:8080'
 @pytest.mark.xray('PRB3103-16')   #comentario
 def test_PRB3103_16_calculo_interes_simple_retorna_valor_correcto(page):
     page.goto(f'{BASE}/dashboard.html')
-    page.screenshot(path=str(EVIDENCIAS / 'PRB16_01_dashboard.png'))
+    page.screenshot(path=str(EVIDENCIAS / 'PRB3103_01_dashboard.png'))
 
     page.fill('#capital', '10000')
     page.fill('#tasa', '5')
     page.fill('#tiempo', '2')
-    page.screenshot(path=str(EVIDENCIAS / 'PRB16_02_datos.png'))
+    page.screenshot(path=str(EVIDENCIAS / 'PRB3103_02_datos.png'))
 
     page.click('button:has-text("Calcular")')
     page.wait_for_timeout(500)
     resultado = page.locator('#res-calculo').text_content()
 
-    page.screenshot(path=str(EVIDENCIAS / 'PRB16_03_resultado.png'))
+    page.screenshot(path=str(EVIDENCIAS / 'PRB3103_03_resultado.png'))
 
     # VALIDACION CORRECTA 1000.00 Incorrecta 999.99
     assert 'Q 1000.00' in resultado, f'Resultado incorrecto: {resultado}'
